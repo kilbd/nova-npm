3c53d95a24835ab6c1875e0a1a989b5d8e4dfe79
Drew Kilb <drew@kilb.com>
2020-09-27 13:34:58 -0400

    test: Round out tests of `inDependencies` method
    Testing to ensure anticipated result if dependency objects do or
    do not exist in package.json.

--- src/completion-assistant.test.ts
+++ src/completion-assistant.test.ts
@@ -1,10 +1,40 @@
 import { NpmCompletionAssistant } from './completion-assistant'
 
 describe('NpmCompletionAssistant', () => {
-  it('works', () => {
-    const assist = new NpmCompletionAssistant()
+  const assist = new NpmCompletionAssistant()
+
+  it('should trigger properly in document with dependencies', () => {
     const doc = '{"name":"test","dependencies":{ }}'
-    const inDependencies = assist.inDependencies(doc, 31)
+    let inDependencies = assist.inDependencies(doc, 31)
     expect(inDependencies).toBeTruthy()
+    inDependencies = assist.inDependencies(doc, 12)
+    expect(inDependencies).toBeFalsy()
+  })
+
+  it('should trigger properly in document with dev dependencies', () => {
+    const doc = '{"name":"test","devDependencies":{ }}'
+    let inDependencies = assist.inDependencies(doc, 34)
+    expect(inDependencies).toBeTruthy()
+    inDependencies = assist.inDependencies(doc, 12)
+    expect(inDependencies).toBeFalsy()
+  })
+
+  it('should trigger in document with both run and dev dependencies', () => {
+    const doc =
+      '{"name":"test","dependencies":{ },"devDependencies":{"jest":"^24.0.6"}}'
+    let inDependencies = assist.inDependencies(doc, 31)
+    expect(inDependencies).toBeTruthy()
+    inDependencies = assist.inDependencies(doc, 56)
+    expect(inDependencies).toBeTruthy()
+    inDependencies = assist.inDependencies(doc, 12)
+    expect(inDependencies).toBeFalsy()
+    inDependencies = assist.inDependencies(doc, 50)
+    expect(inDependencies).toBeFalsy()
+  })
+
+  it('should not trigger in document with no dependencies', () => {
+    const doc = '{"name":"test","scripts":{"run":"node main.js"}}'
+    let inDependencies = assist.inDependencies(doc, 31)
+    expect(inDependencies).toBeFalsy()
   })
 })
